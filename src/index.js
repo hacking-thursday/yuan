a3368e28ed84f077ba13942360f6f15263bea6ba
Kuo-tung Kao <ya790206@gmail.com>
2018-01-20 23:30:25 +0800

    add router support

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const component = ReactDOM.render(<App a="s" />, document.getElementById('root'));
-setTimeout(() => {
-  component.setState({
-    heading: 'React Awesomesauce',
-  });
-}, 3000);
+
+const component = ReactDOM.render((
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App}/>
+    </div>
+  </BrowserRouter>
+
+), document.getElementById('root'));
 
 registerServiceWorker();
 
