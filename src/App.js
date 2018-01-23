9227c6c31cf92568e793daf8a178e864eb450fbd
Kuo-tung Kao <ya790206@gmail.com>
2018-01-23 22:39:27 +0800

    format code and fix for eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import {
-  Grid,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
 
@@ -13,27 +9,33 @@ class ProjectCell extends Component {
   render() {
     const data = this.props.data
     const skills = data.skills.map(i => {
-      return <span><span class="badge badge-pill badge-info">{i}</span> </span>
+      return <span key={ i }><span key={ i } className='badge badge-pill badge-info'>{ i }</span> </span>
     })
-    console.log(data)
     const domains = data.domains.map(i => {
-      return <span><span class="badge badge-warning">{i}</span> </span>
+      return <span key={ i }><span className='badge badge-warning'>{ i }</span> </span>
     })
 
     const links = data.links.map(i => {
-      return (<a href={i.url}><img src={i.icon} alt={i.title}></img> </a>)
+      return (<a key={ i } href={ i.url }><img src={ i.icon } alt={ i.title }></img> </a>)
     })
 
     return (
-      <div class="card">
-        <div class="card-body">
-          <a href={data.entry}><img class="card-img-top" src={data.cover} alt="Card cap"/></a>
-          <h5 class="card-title"><a href={data.entry}>{data.name}</a></h5>
-          <p class="card-text">{data.description}</p>
-          <div>{skills}</div>
-          <div>{domains}</div>
-          <div>{links}</div>
-
+      <div className='card'>
+        <div className='card-body'>
+          <a href={ data.entry }><img className='card-img-top' src={ data.cover } alt='Card cap'/></a>
+          <h5 className='card-title'><a href={ data.entry }>{ data.name }</a></h5>
+          <p className='card-text'>
+            { data.description }
+          </p>
+          <div>
+            { skills }
+          </div>
+          <div>
+            { domains }
+          </div>
+          <div>
+            { links }
+          </div>
         </div>
       </div>
     )
@@ -42,28 +44,26 @@ class ProjectCell extends Component {
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       data: {}
     };
 
-    axios.get("https://raw.githubusercontent.com/hacking-thursday/yuan-data/master/autogen/data.json")
+    axios.get('https://raw.githubusercontent.com/hacking-thursday/yuan-data/master/autogen/data.json')
       .then(res => {
         this.setState({
           data: res.data
         })
         this.forceUpdate()
-        console.log(res)
-        console.log(this.state.data)
       });
 
   }
 
   getJumbotron() {
-    return(
-      <div class="jumbotron">
-        <h1 class="display-3">Yuan</h1>
-        <p class="lead">增加開放原始碼專案效益，媒合提案者與貢獻者，建立良好的軟體開發流程。</p>
+    return (
+      <div className='jumbotron'>
+        <h1 className='display-3'>Yuan</h1>
+        <p className='lead'>增加開放原始碼專案效益，媒合提案者與貢獻者，建立良好的軟體開發流程。</p>
       </div>
     )
   }
@@ -71,29 +71,35 @@ class App extends Component {
   getHeader() {
     const jumbotron = this.getJumbotron()
     return (<Row>
-              <Col sm={12} md={12} lg={12}>
-                {jumbotron}
+              <Col sm={ 12 } md={ 12 } lg={ 12 }>
+                { jumbotron }
               </Col>
             </Row>)
   }
 
   getProjects() {
-    if( this.state.data.projects === undefined) {
+    if (this.state.data.projects === undefined) {
       return <Row></Row>
     }
     var projects = this.state.data.projects
     const rows = []
     var cols = []
-    for (var i = 0; i < projects.length; i++){
+    for (var i = 0; i < projects.length; i++) {
       cols.push(
-        <Col sm={3} md={3} lg={3}> <ProjectCell data={projects[i]}></ProjectCell> </Col>
+        <Col sm={ 3 } md={ 3 } lg={ 3 }>
+          <ProjectCell data={ projects[i] }></ProjectCell>
+        </Col>
       )
-      if ( i % 4 === 3 ){
-        rows.push(<Row>{cols}</Row>)
+      if (i % 4 === 3) {
+        rows.push(<Row>
+                    { cols }
+                  </Row>)
         cols = []
       }
     }
-    rows.push(<Row>{cols}</Row>)
+    rows.push(<Row>
+                { cols }
+              </Row>)
     return rows
   }
 
@@ -103,11 +109,11 @@ class App extends Component {
 
 
     return (
-        <Grid>
-          {header}
-          {rows}
-        </Grid>
-    );
+      <Grid>
+        { header }
+        { rows }
+      </Grid>
+      );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import MyNavbar from './MyNavbar.js'
 import registerServiceWorker from './registerServiceWorker';
 
 
-const component = ReactDOM.render((
+ReactDOM.render((
   <p>
     <MyNavbar/>
     <BrowserRouter>
       <Switch>
-        <Route component={App}/>
+        <Route component={ App } />
       </Switch>
     </BrowserRouter>
   </p>
 
-), document.getElementById('root'));
+  ), document.getElementById('root'));
 
 registerServiceWorker();
 
